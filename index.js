9c75ded884d985d6990850794387a261df7a8c6c
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-08-18 21:05:49 +0530

    adding the dynamic routing to the page

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/profile/:username", (req, res) => {
+  user = req.params.username;
+  res.send(`Its working ${user}`);
+});
+
+app.get("/profile/:username/:age", (req, res) => {
+  user = req.params.username;
+  age = req.params.age;
+  res.send(`Its working ${user} of age ${age}`);
+});
+
 app.listen(3000, function () {
   console.log("its listning in port 3000");
 });
