1b64812d260caa8cd696b75d9da0745cb117f44d
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-08-18 20:31:45 +0530

    installed EJS ad added the views folder

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.render("index");
 });
 
 app.listen(3000, function () {
