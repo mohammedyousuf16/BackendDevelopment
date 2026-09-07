f47cbdcd69557af755ef941cccf70f719346fc21
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-09-07 20:51:37 +0530

    creating file and viewing the file added

--- Key concepts/index.js
+++ Key concepts/index.js
@@ -1,15 +1,44 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
+const { log } = require("console");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("welcome my BOY!");
+  fs.readdir(`./files`, function (err, files) {
+    res.render("index", { files: files });
+  });
+});
+
+app.get("/files/:filename", function (req, res) {
+  fs.readFile(
+    `./files/${req.params.filename}`,
+    "utf-8",
+    function (err, filedata) {
+      res.render("show", {
+        filename: req.params.filename,
+        filedata: filedata,
+      });
+    },
+  );
+});
+
+app.post("/create", (req, res) => {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.details,
+    function (err) {
+      if (err) {
+        console.log(err);
+      }
+    },
+  );
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
