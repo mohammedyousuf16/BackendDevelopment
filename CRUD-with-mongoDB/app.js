d545d4a1182e524ef4ca0a0107cbc659df7390d1
mohammedyousuf16 <mohammedyousuf16.md@gmail.com>
2026-09-22 19:25:46 +0530

    new create route for creating auser in DB

--- CRUD-with-mongoDB/app.js
+++ CRUD-with-mongoDB/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const userModel = require("./usermodel");
 
 app.get("/", (req, res) => {
   res.send("hello");
+});
+
+app.get("/create", async (req, res) => {
+  const Createduser = await userModel.create({
+    name: "yousuf",
+    username: "mdyousuf",
+    email: "yousufmd1@gamil.com",
+  });
+  res.send(Createduser);
 });
 
 app.listen(3000, () => {
